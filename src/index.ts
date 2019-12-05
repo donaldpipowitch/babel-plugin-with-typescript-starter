443e8ff0cb50e78d5259e368873811c1896fa17b
Donald Pipowitch <pipo@senaeh.de>
2019-12-05 10:44:50 +0100

    added t

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
-import { PluginObj } from '@babel/core';
+import babel, { PluginObj } from '@babel/core';
 
-export default function(): PluginObj {
+// also not needed in this example `t` is something you'll need in many plugins
+// see https://github.com/jamiebuilds/babel-handbook/blob/master/translations/en/plugin-handbook.md#writing-your-first-babel-plugin
+export default function({ types: t }: typeof babel): PluginObj {
   return {
     visitor: {
       Identifier(path) {
